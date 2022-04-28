c981c534ada5e7087f2f2e1683ecb86e5247f112
guangzheng.li <guangzheng.li@seamoney.com>
2022-04-28 11:22:55 +0800

    add post sort

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { useAppSelector } from '../../app/hooks';
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor';
+import { PostsInterface } from './postsSlice';
+import { TimeAgo } from './TimeAgo';
 
 export const PostsList = () => {
-  const posts = useAppSelector(state => state.posts);
+  const posts: PostsInterface[] = useAppSelector(state => state.posts);
+  const orderdPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = posts.map(post => (
+  const renderedPosts = orderdPosts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
-      <p><PostAuthor userId={post.user}/></p>
+      <p><PostAuthor userId={post.user}/><TimeAgo timestamp={post.date} /></p>
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <Link to={`/posts/${post.id}`} className="button muted-button">View Post</Link>
     </article>
--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,15 +1,17 @@
 import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
-interface PostsInterface {
+export interface PostsInterface {
   id: string,
+  date: string,
   title: string,
   content: string,
   user: string
 }
 
 const initialState: PostsInterface[] = [
-  { id: '1', title: 'First Post!', content: 'Hello!', user: '0' },
-  { id: '2', title: 'Second Post', content: 'More text', user: '1' }
+  { id: '1', title: 'First Post!', content: 'Hello!', user: '0', date: sub(new Date(), { minutes: 10}).toISOString() },
+  { id: '2', title: 'Second Post', content: 'More text', user: '1', date: sub(new Date(), { minutes: 5}).toISOString() }
 ]
 
 const postsSlice = createSlice({
@@ -24,6 +26,7 @@ const postsSlice = createSlice({
         return {
           payload: {
             id: nanoid(),
+            date: new Date().toISOString(),
             title,
             content,
             user: userId
